79baf7be0a5f63504cb6b2bb3481ca586c07e551
sawyehtet <sawyehtet845@gmail.com>
2023-06-20 21:39:46 +0630

    Laravel Sanctum with User Controller

---
+++ app/Http/Controllers/API/UserController.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Models\User;
+use App\Http\Controllers\Controller;
+use App\Http\Requests\User\UpdateUserRequest;
+
+class UserController extends Controller
+{
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(int $id)
+    {
+        try{
+            $user = User::findOrFail($id);
+            return response()->json(['user' => $user],200);
+        }catch(\Exception $e){
+            return response()->json([
+                'error' => 'Something went wrong in
+                UserController.show',
+                'error' => $e->getMessage(),
+
+            ]);
+        }
+    }
+
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateUserRequest $request, string $id)
+    {
+        try{
+            $user = User::findOrFail($id);
+            $user->first_name = $request->first_name;
+            $user->last_name = $request->last_name;
+            // $user->email = $request->email;
+            // $user->description = $request->description;
+
+            $user->save();
+            return response()->json('User details updated',200);
+        }catch(\Exception $e){
+            return response()->json([
+                'error' => 'Something went wrong in
+                UserController.update',
+                'error' => $e->getMessage(),
+
+            ]);
+        }
+    }
+}
