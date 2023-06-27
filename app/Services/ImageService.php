db95a05e620dc4a5e2faa2214b5210fc162dd034
sawyehtet <sawyehtet845@gmail.com>
2023-06-27 13:25:53 +0630

    Laravel Intervention Image with ImageService

---
+++ app/Services/ImageService.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Services;
+
+use Intervention\Image\Facades\Image;
+
+
+
+
+class ImageService {
+    public function updateImage($model, $request , $path, $methodType) {
+        $image = Image::make($request->file('image'));
+
+        if(!empty($model->image)) {
+            $currentImage = public_path() . $path . $model->image;
+
+            if(file_exists($currentImage)) {
+                unlink($currentImage);
+            }
+        }
+
+        $file = $request->file('image');
+        $extension = $file->getClientOriginalExtension();
+
+        $image->crop(
+            $request->width,
+            $request->height,
+            $request->left,
+            $request->top,
+        );
+
+        $name = time() . '.' . $extension;
+        $image->save(public_path() . $path . $name);
+
+        if($methodType === 'store') {
+            $model->user_id = $request->get('user_id');
+        }
+        $model->image = $name;
+        $model->save();
+    }
+}
+
+
